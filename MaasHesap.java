1bc1f77c8d972c512dfabc67ca91fb4271e548b9
Miraç Duran <mduran2341@gmail.com>
2025-11-02 13:23:37 +0300

    Remove brackets from personal information comments

--- MaasHesap.java
+++ MaasHesap.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [Mirac Duran]
- * Ogrenci No: [250541091]
- * Tarih: [2.11.2025]
+ * Ad Soyad: Mirac Duran
+ * Ogrenci No: 250541091
+ * Tarih: 2.11.2025
  * Aciklama: Maas Hesaplama Sistemi
  */
 
