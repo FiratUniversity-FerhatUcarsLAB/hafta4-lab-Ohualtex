b1e95edab0cbb682b7b6b01c60ecd1fa80c28b07
Miraç Duran <mduran2341@gmail.com>
2025-11-02 12:09:59 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,36 @@
+/*
+ * Ad Soyad: [Mirac Duran]
+ * Ogrenci No: [2505411091]
+ * Tarih: [2.11.2025]
+ * Aciklama: Geometrik Hesaplayici
+ */
+
+import java.util.Scanner;
+
+public class GeometrikHesap {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== GEOMETRIK HESAPLAYICI ===");
+        System.out.print("Dairenin yaricapini girin (cm): ");
+        double r = input.nextDouble();
+
+        double daireAlani = PI * Math.pow(r, 2);
+        double daireCevresi = 2 * PI * r;
+        double daireCapi = 2 * r;
+        double kureHacmi = (4.0 / 3.0) * PI * Math.pow(r, 3);
+        double kureYuzeyAlani = 4 * PI * Math.pow(r, 2);
+
+        System.out.println("\nSONUCLAR:");
+        System.out.println("----------");
+        System.out.printf("Daire Alani        : %.2f cm²\n", daireAlani);
+        System.out.printf("Daire Cevresi      : %.2f cm\n", daireCevresi);
+        System.out.printf("Daire Capi         : %.2f cm\n", daireCapi);
+        System.out.printf("Kure Hacmi         : %.2f cm³\n", kureHacmi);
+        System.out.printf("Kure Yuzey Alani   : %.2f cm²\n", kureYuzeyAlani);
+
+        input.close();
+    }
+}
 
